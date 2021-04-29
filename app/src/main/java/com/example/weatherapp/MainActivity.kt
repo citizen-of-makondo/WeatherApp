2c3fefd3e1e67a4b9cbfa059202b86985efe5c29
Alex <a_v_ilinskiy@mail.ru>
2021-04-30 01:47:03 +0500

    Create 3 loops, show data from array in Logcat

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.Log.INFO
 import android.widget.Button
 import android.widget.TextView
+import java.util.logging.Level.INFO
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,6 +24,19 @@ class MainActivity : AppCompatActivity() {
             textViewInt.setText(data.intField.toString())
         }
 
+        val array = arrayOf(1, 2, 3, 4, 5)
+
+        for (i in array.iterator()) {
+            println(i)
+        }
+
+        array.forEach {
+            println("Loop from forEach $it")
+        }
+
+        for (i in array) {
+            println("Easy loop $i")
+        }
 
     }
 }
