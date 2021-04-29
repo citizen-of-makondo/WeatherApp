7530ad125dcc69e7cb16b477981eb43a51e264cd
Alex <a_v_ilinskiy@mail.ru>
2021-04-30 00:59:34 +0500

    Create 2 textView and data class

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -9,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textViewString = findViewById<TextView>(R.id.textViewString)
+        val textViewInt = findViewById<TextView>(R.id.textViewInt)
+
+        val data = TestClass()
+        textViewString.setText(data.stringField)
+        textViewInt.setText(data.intField)
     }
 }
